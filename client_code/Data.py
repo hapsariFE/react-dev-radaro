110294ce9e30081f316a42f29d882a1fde46246f
danny.degen@radaro.com.au <danny.degen@radaro.com.au>
2023-06-19 01:50:38 +0000

    Created Data, Homepage, Mainpage, Mainpage.Escalationlist and 4 more; Edited settings; Deleted Form1

---
+++ client_code/Data.py
@@ -0,0 +1,13 @@
+"""This module collects global variables to be used throughout the app"""
+import anvil.server
+#import anvil.google.auth, anvil.google.drive
+#from anvil.google.drive import app_files
+#import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+esc_status = [(x['name'], x) for x in app_tables.escalation_status.search()]
+esc_type = [(x['name'], x) for x in app_tables.escalation_type.search()]
+job_status = [(x['name'], x) for x in app_tables.job_status.search()]
+merchant_name = [(x['name'], x) for x in app_tables.merchant.search()]
---
+++ client_code/Mainpage/Mainpage/__init__.py
@@ -0,0 +1,32 @@
+from ._anvil_designer import MainpageTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from ... import Data
+from anvil.tables import app_tables
+from datetime import datetime, timedelta, date
+
+class Mainpage(MainpageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.filters = {}
+    self.date_filters = {} 
+    #users = anvil.server.call('get_users')
+    #self.users = [(x['name'], x) for x in users]
+    self.users = 'Danny'
+    self.esc_status = Data.esc_status
+    self.esc_type = Data.esc_type
+    self.job_status = Data.job_status
+    self.merchant_name = Data.merchant_name
+
+    
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def initialise_start_dates(self):
+    """Initialise the DatePickers so that the filter auto-displays data for the previous week"""
+    self.date_filters['start'] = (date.today() - timedelta(days=6))
+    self.date_filters['end'] = date.today()
