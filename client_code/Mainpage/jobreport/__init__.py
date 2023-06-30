b8f09beca2c8da599498587253e550f1211b7fc8
matthew.santilli@radaro.com.au <matthew.santilli@radaro.com.au>
2023-06-30 01:13:19 +0000

    Edited Mainpage.jobreport

--- client_code/Mainpage/jobreport/__init__.py
+++ client_code/Mainpage/jobreport/__init__.py
@@ -5,10 +5,19 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil.js.window import jQuery
+from anvil.js import get_dom_node
 
 class jobreport(jobreportTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    self.xy_panel_1 = None
     self.init_components(**properties)
+
+  iframe = jQuery("<iframe width='100%' height='800px'>").attr("src","https://www.wired.co.uk/")
+  iframe.appendTo(get_dom_node(self.xy_panel_1))
+
+
+
 
     # Any code you write here will run before the form opens.
