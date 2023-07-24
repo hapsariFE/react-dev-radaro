e2c5422b361e53bbd26736be2ae36e2e97b7d543
danny.degen@radaro.com.au <danny.degen@radaro.com.au>
2023-07-24 07:25:42 +0000

    Edited theme.css, Homepage, standard-page.html and Mainpage.Mainpage

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -4,12 +4,30 @@ import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
+from ..Data import *
 from anvil.tables import app_tables
+from datetime import datetime, timedelta, date
 
 
 class Homepage(HomepageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    self.filters = {}
+    self.date_filters = {} 
+    #users = anvil.server.call('get_user_list')
+    self.users = ""
+    #self.users = 'Danny'
+    self.esc_status = esc_status
+    self.esc_type = esc_type
+    self.job_status = job_status
+    self.merchant_name = ""
+    startDate= (date.today() - timedelta(days=60))
+    endDate = date.today()
+    escType= None
+    self.assigned = ""
+    assigned_to = None
+
+    
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
