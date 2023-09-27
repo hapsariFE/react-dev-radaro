846595abe6fa53af419dbaa79d94d4e665208c4d
matthew.santilli@radaro.com.au <matthew.santilli@radaro.com.au>
2023-09-27 23:38:34 +0000

    Edited CMS, CMS.RowTemplate4, Data, Homepage and 7 more

--- client_code/Modal_wide/RowTemplate2/__init__.py
+++ client_code/Modal_wide/RowTemplate2/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import RowTemplate2Template
 from anvil import *
+import anvil.microsoft.auth
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.server
