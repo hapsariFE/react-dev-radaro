219a37a6bc4bac9cd0ed64e3cb4d4cd317617b1c
danny.degen@radaro.com.au <danny.degen@radaro.com.au>
2023-10-13 04:21:11 +0000

    Edited Reporting

--- client_code/Reporting/__init__.py
+++ client_code/Reporting/__init__.py
@@ -22,17 +22,17 @@ class Reporting(ReportingTemplate):
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
     
-    create_chart1 = anvil.server.call('create_chart1') 
-    self.plot_1.figure = create_chart1
+    #create_chart1 = anvil.server.call('create_chart1') 
+    #self.plot_1.figure = create_chart1
 
-    create_chart2 = anvil.server.call('create_chart2') 
-    self.plot_2.figure = create_chart2
+    #create_chart2 = anvil.server.call('create_chart2') 
+    #self.plot_2.figure = create_chart2
 
     create_chart3 = anvil.server.call('create_chart3') 
     self.plot_3.figure = create_chart3
-
-    create_chart4 = anvil.server.call('create_chart4') 
-    self.plot_4.figure = create_chart4
+        
+    #create_chart4 = anvil.server.call('create_chart4') 
+    #self.plot_4.figure = create_chart4
   
 
 
@@ -41,3 +41,8 @@ class Reporting(ReportingTemplate):
   def Dashboard_click(self, **event_args):
 
     open_form('Homepage')
+
+  def call_js(self, **event_args):
+    """This method is called when the Plot is shown on the screen"""
+    self.call_js('buildChart')
+    pass
