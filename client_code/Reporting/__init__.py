7babf86c4002081d4368e4e352c98a476548a906
danny.degen@radaro.com.au <danny.degen@radaro.com.au>
2023-10-19 23:16:50 +0000

    Edited Reporting

--- client_code/Reporting/__init__.py
+++ client_code/Reporting/__init__.py
@@ -21,25 +21,31 @@ class Reporting(ReportingTemplate):
 
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
-
+    #pie chart last week
     create_stat1 = anvil.server.call('create_stat1') 
     self.plot_6.figure = create_stat1
 
+    #bar chart last week vs this week
     create_stat2 = anvil.server.call('create_stat2') 
     self.plot_7.figure = create_stat2
-    
+
+    #response by user
     create_chart1 = anvil.server.call('create_chart1') 
     self.plot_1.figure = create_chart1
 
+    #tickets by escalation type
     create_chart2 = anvil.server.call('create_chart2') 
     self.plot_2.figure = create_chart2
 
+    #tickets by creation date
     create_chart3 = anvil.server.call('create_chart3') 
     self.plot_3.figure = create_chart3
-        
-    create_chart4 = anvil.server.call('create_chart4') 
-    self.plot_4.figure = create_chart4
-  
+    
+    #        
+    #create_chart4 = anvil.server.call('create_chart4') 
+    #self.plot_4.figure = create_chart4
+
+    #tickets by creation date
     create_chart5 = anvil.server.call('create_chart5') 
     self.plot_5.figure = create_chart5
 
